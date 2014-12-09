2bbaf4ff96fcbf7132b5af97c7088f1f9a037efc
Hadi <hadichalabi@gmail.com>
2014-12-09 19:19:47 +0000

    private tasks functional

--- simple-todos.js
+++ simple-todos.js
@@ -1,6 +1,7 @@
 Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
+  Meteor.subscribe("tasks");
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
@@ -53,6 +54,17 @@ if (Meteor.isClient) {
 
     "click .delete": function(){
       Meteor.call("deleteTask", this._id);
+    },
+
+    "click .toggle-private": function(){
+      Meteor.call("setPrivate", this._id, ! this.private);
+    }
+  });
+
+
+  Template.task.helpers({
+    isOwner: function(){
+      return this.owner === Meteor.userId();
     }
   });
 }
@@ -76,6 +88,29 @@ Meteor.methods({
   },
 
   setChecked: function(taskId, setChecked){
-    Tasks.update(taskId, {$set: { checked: setChecked}});
+    Tasks.update(taskId, {$set: { private: setToPrivate}});
+  },
+
+  setPrivate: function (taskId, setToPrivate) {
+    var task = Tasks.findOne(taskId);
+
+    // Make sure only the task owner can make a task private
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
   }
+
 });
+
+if(Meteor.isServer){
+  Meteor.publish("tasks", function () {
+    return Tasks.find({
+      $or: [
+      { private: {$ne: true} },
+      { owner: this.userId }
+      ]
+    });
+  });
+}
